76d51844a3b58675c2f4a933e4759ce2b3dcd36c
Richard Walker <richard.walker.90@gmail.com>
2015-04-16 09:37:33 -0400

    Making hasRoute method

--- src/graph/GraphTester.java
+++ src/graph/GraphTester.java
@@ -22,10 +22,17 @@ public class GraphTester {
 		graph.addEdge("1000", "BOS", "JFK", true);
 		graph.addEdge("2000", "JFK", "ATL", true);
 		graph.addEdge("2001", "ATL", "JFK", true);
+		graph.addEdge("2002", "JFK", "ATL", true);
 		graph.addEdge("3000", "ATL", "BOS", true);
 		graph.addEdge("4000", "ATL", "MIA", true);
 		
-		/* Exploring Graph */
+		/* Nodes */
+		Node bos = graph.getNode("BOS");
+		Node jfk = graph.getNode("JFK");
+		Node atl = graph.getNode("ATL");
+		Node mia = graph.getNode("MIA");
+		
+		/* Edges */
 		Edge flight1000 = graph.getEdge("1000");
 		flight1000.addAttribute("depTime", "12:00");
 		
@@ -34,6 +41,9 @@ public class GraphTester {
 		
 		Edge flight2001 = graph.getEdge("2001");
 		flight2001.addAttribute("depTime", "14:00");
+		
+		Edge flight2002 = graph.getEdge("2002");
+		flight2002.addAttribute("depTime", "15:00");
 		
 		/* Verifying Graph Data */
 		System.out.println("The flight number is: " + flight1000.getId());
@@ -51,9 +61,26 @@ public class GraphTester {
 		System.out.println("The flight time is : " + flight2001.getAttribute("depTime"));
 		System.out.println("Is this edge directed? " + flight2001.isDirected());
 		
+		System.out.println("The flight number is: " + flight2002.getId());
+		System.out.println("The flight has : " + flight2002.getAttributeCount() + " attribute");
+		System.out.println("The flight time is : " + flight2002.getAttribute("depTime"));
+		System.out.println("Is this edge directed? " + flight2002.isDirected());
+		
+		System.out.println("BOS -> ATL? : " + bos.hasEdgeFrom(jfk)); // BOS -> ATL No!
+		System.out.println("BOS -> JFK? : " + bos.hasEdgeFrom(atl)); // BOS -> JFK Yes!
+		
+		System.out.println("Theres a route between " + bos + " and " + atl + ": " + hasRoute(bos,atl)); 
+		
 		graph.display();
 
 	}
 	
+	/* Check if there's a route */
+	private static boolean hasRoute(Node depNode, Node arrNode){
+		
+		System.out.println("Arrive same as depart " + depNode.equals(arrNode));
+		return true;
+		
+	}
 	
 }
