c2b01adcef9e06dffb71ea7673f3cc2e25b9a1e8
Richard Walker <richard.walker.90@gmail.com>
2015-04-24 21:10:44 -0400

    Added a user_interface package

---
+++ src/user_interface/ErrorHandler.java
@@ -0,0 +1,67 @@
+package user_interface;
+
+/**
+ * Class used to display the potential error messages, 
+ * which may be caused by inappropriate user input. 
+ * 
+ * @author Richard Walker
+ *
+ */
+
+public class ErrorHandler {
+	
+	/* Prevents this class from being instantiated */
+	private ErrorHandler() {}
+	
+	/**
+	 * Prints an error message that states that the
+	 * user tried to input an invalid airport.
+	 */
+	public static void invalidPort(){
+		System.out.println("That is not a valid Airport. ");	
+
+	}
+	
+	/**
+	 * Prints an error message that states that the
+	 * departure airport can't be the same as the arrival.
+	 */
+	public static void samePort(){
+		System.out.println("Arrival Aiport can't be the same as the Departure Airport!");
+	}
+	
+	/**
+	 * Prints an error message that states that the
+	 * user tried to input an invalid date.
+	 */
+	public static void invalidDate(){
+		System.out.println("That is not a valid Date");
+	}
+		
+	/**
+	 * Prints an error message that states the user tried 
+	 * to select an invalid flight.
+	 */
+	public static void invalidFlight(){
+		System.out.println("That is not a valid flight");
+	}
+	
+	/**
+	 * Prints an error message that states that the
+	 * user tried to input a non-numeric character 
+	 * for the date (day).
+	 */
+	public static void notANum(){
+		System.out.println("That is not a number!");
+	}
+	
+	/**
+	 * Prints an error message that states that the
+	 * user did not type "Yes" or "No" for their answer.
+	 */
+	public static void notYesOrNo(){
+		System.out.println("Please type Y or N");
+	}
+	
+	
+}
---
+++ src/user_interface/IUserInterface.java
@@ -0,0 +1,75 @@
+package user_interface;
+
+/** 
+ * Interface used to specify what a user interface for the flight system 
+ * needs to at least do. Used to abstract the model (flight system) from the view
+ * (the user interface). 
+ * <p>
+ * 
+ * @author Richard Walker
+ */
+public interface IUserInterface {
+	
+	/**
+	 * Displays the welcome message.
+	 */
+	public void displayWelcomeMsg();
+	
+	/**
+	 * Used to ask the user for the departure airport's 3-digit code.
+	 * @return the departure airport as a string from the user.
+	 */
+	public String getDepartureAirport();
+	
+	/**
+	 * Used to ask the user for the arrival airport's 3-digit code.
+	 * @return the arrival airport as a string from the user.
+	 */
+	public String getArrivalAirport();
+	
+	/**
+	 * Used to ask the user what date they want to leave.
+	 * @return the departure date (day) as a string from the user.
+	 */
+	public String getLeavingDate();
+	
+	/**
+	 * Used to ask the user what date they want to return.
+	 * @return the return date (day) as a string from the user.
+	 */
+	public String getReturnDate();
+	
+	/**
+	 * Used to ask the user if they want 1st class seating or not.
+	 * <p>
+	 * The user answer should start with a 'y' for yes
+	 * and a 'n' for no.
+	 * @return the "yes" or "no" as a string.
+	 */
+	public String isFirstClassSeat();
+	
+	/**
+	 * Displays a message that tells the user how many flights 
+	 * fit their search criteria.
+	 * @param numOfFlights the number of available flights
+	 */
+	public void numOfFlights(int numOfFlights);
+	
+	/**
+	 * Displays a message that tells the user there are no flights 
+	 * that fit their search criteria.
+	 */
+	public void noFlights();
+	
+	/**
+	 * Asks the user which flight they would like to book.
+	 */
+	public String bookFlight();
+	
+	/**
+	 * Asks the user if they would like to book another flight.
+	 */
+	public String bookAnother();
+	
+	
+}
